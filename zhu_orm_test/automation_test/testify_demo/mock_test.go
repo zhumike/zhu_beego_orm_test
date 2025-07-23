136c3c9186f80de4bd9a0e6fe88cd04fe0f6ff2d
zhenzhongzhu <zhuzhenzhong@yuewen.com>
2025-07-23 18:39:03 +0800

    更新校验点

--- zhu_orm_test/automation_test/testify_demo/mock_test.go
+++ zhu_orm_test/automation_test/testify_demo/mock_test.go
@@ -24,18 +24,28 @@ func (m *MyMockedObj) DoSomething(number int) (bool, error) {
 	return args.Bool(0), args.Error(1)
 }
 
+func (m *MyMockedObj) GetOrderNumberInfo(orderId int) (int, error) {
+	args := m.Called(orderId)
+	return args.Int(0), args.Error(1)
+}
+
 func TestSomething(t *testing.T) {
 	//实例化
 	testObj := new(MyMockedObj)
 
 	//设置mock期望方法，当入参是100，返回true
 	testObj.On("DoSomething", 100).Return(true, nil)
+	testObj.On("GetOrderNumberInfo", 100).Return(1001200, nil)
 
 	//调用mock对象的方法
 	something, err := testObj.DoSomething(100)
+	orderNumber, _ := testObj.GetOrderNumberInfo(100)
 
 	//断言
 	assert.NoError(t, err)
 	assert.True(t, something)
+	assert.Equal(t, 1001200, orderNumber)
+	assert.Greater(t, orderNumber, 0)
+	assert.NotEqual(t, 230, orderNumber)
 	testObj.AssertExpectations(t)
 }
